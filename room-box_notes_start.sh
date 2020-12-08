0664d00d2bbbabb16bdf8ecaa9ca9fbd905af016
Labruhtooryboi <67720330+Labruhtooryboi@users.noreply.github.com>
2020-12-08 10:43:56 -0900

    Add files via upload

---
+++ room-box_notes_start.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+
+subl /opt/Shorts/ctf_quickie/revshells.txt
+subl /opt/Shorts/ctf_quickie/koth-specific/metwis-lin.txt
+subl /opt/Shorts/ctf_quickie/koth-specific/metwis-win.txt
+subl /opt/Shorts/templates/wargames-reports.txt
+subl notes.txt
+subl readme.txt
+mkdir recon
+mkdir enum
+mkdir exploitation
