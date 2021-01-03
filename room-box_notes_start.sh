902f6ed2820d9b2b7d97c0b76e248ad6a1b774cc
Labruhtooryboi <67720330+Labruhtooryboi@users.noreply.github.com>
2021-01-02 20:00:02 -0900

    Update room-box_notes_start.sh

--- room-box_notes_start.sh
+++ room-box_notes_start.sh
@@ -4,7 +4,7 @@
 subl /opt/Wargames/ctf_quickie/revshells.txt
 subl /opt/Wargames/ctf_quickie/koth-specific/metwis-lin.txt
 subl /opt/Wargames/ctf_quickie/koth-specific/metwis-win.txt
-subl /opt/Wargames/templates/wargames-reports.txt
+subl /opt/Wargames/report-template-1.0.txt
 subl notes.txt
 subl readme.txt
 mkdir recon
