95f9cad7c2d3e9f7b507efe759b41b8c5716973d
Labruhtooryboi <labruhtooryboi@gmail.com>
2021-01-16 18:36:06 -0800

    Update room-box_notes_start.sh

--- room-box_notes_start.sh
+++ room-box_notes_start.sh
@@ -5,6 +5,7 @@ subl notes.txt
 subl readme.txt
 subl /opt/Wargames/ctf_quickie/report-template-1.0.txt
 subl /opt/Wargames/ctf_quickie/revshells.txt
+subl /opt/Wargames/ctf_quickie/revshells2.0.txt
 subl /opt/Wargames/ctf_quickie/koth-specific/metwis-lin.txt
 subl /opt/Wargames/ctf_quickie/koth-specific/metwis-win.txt
 mkdir recon
